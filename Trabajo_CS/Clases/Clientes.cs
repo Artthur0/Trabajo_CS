782c1cb91a4b628abffd7353bf5a9ad0719eb547
[Arthur] <115912843+Artthur0@users.noreply.github.com>
2024-09-11 18:02:18 -0300

    Agregar archivos de proyecto.

---
+++ Trabajo_CS/Clases/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_CS.Clases
+{
+    internal class Clientes
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public decimal DineroD { get; set; }
+
+
+       
+    }
+}
---
+++ Trabajo_CS/Clases/Vehiculos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_CS.Clases
+{
+    internal class Vehiculos
+    {
+        public string Marca { get; set; }
+        public decimal Año { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
