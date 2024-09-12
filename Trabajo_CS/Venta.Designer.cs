b3f01befd631f7247f04348fe35a5de924ce4454
[Arthur] <115912843+Artthur0@users.noreply.github.com>
2024-09-11 22:45:16 -0300

    Avance

--- Trabajo_CS/Venta.Designer.cs
+++ Trabajo_CS/Venta.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(678, 415);
+            this.button1.Location = new System.Drawing.Point(361, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -82,7 +82,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 153);
+            this.label3.Location = new System.Drawing.Point(63, 169);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(94, 16);
             this.label3.TabIndex = 5;
@@ -92,7 +92,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(564, 450);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cbxvehiculo);
             this.Controls.Add(this.cbxcliente);
