6e2d23cbe14ac6c07cdbc125e2ba5d0cd5e81d8c
[Arthur] <115912843+Artthur0@users.noreply.github.com>
2024-09-11 22:20:28 -0300

    verificación

--- Trabajo_CS/Venta.Designer.cs
+++ Trabajo_CS/Venta.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxcliente = new System.Windows.Forms.ComboBox();
+            this.cbxvehiculo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(678, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seleccione al Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(292, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Seleccione el Vehiculo";
+            // 
+            // cbxcliente
+            // 
+            this.cbxcliente.FormattingEnabled = true;
+            this.cbxcliente.Location = new System.Drawing.Point(38, 65);
+            this.cbxcliente.Name = "cbxcliente";
+            this.cbxcliente.Size = new System.Drawing.Size(160, 24);
+            this.cbxcliente.TabIndex = 3;
+            // 
+            // cbxvehiculo
+            // 
+            this.cbxvehiculo.FormattingEnabled = true;
+            this.cbxvehiculo.Location = new System.Drawing.Point(279, 65);
+            this.cbxvehiculo.Name = "cbxvehiculo";
+            this.cbxvehiculo.Size = new System.Drawing.Size(166, 24);
+            this.cbxvehiculo.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ingrese Precio";
+            // 
+            // Venta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxvehiculo);
+            this.Controls.Add(this.cbxcliente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Venta";
             this.Text = "Venta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxcliente;
+        private System.Windows.Forms.ComboBox cbxvehiculo;
+        private System.Windows.Forms.Label label3;
     }
 }
