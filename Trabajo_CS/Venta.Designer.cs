b3c8819433430e8947d11ff17e33a8d83f69dd08
Kushad <matias.poveda@virginiogomez.cl>
2024-09-11 23:04:04 -0300

    Ventas 0.6

--- Trabajo_CS/Venta.Designer.cs
+++ Trabajo_CS/Venta.Designer.cs
@@ -34,71 +34,99 @@
             this.cbxcliente = new System.Windows.Forms.ComboBox();
             this.cbxvehiculo = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(361, 162);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(232, 121);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(67, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Guardar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 36);
+            this.label1.Location = new System.Drawing.Point(38, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 16);
+            this.label1.Size = new System.Drawing.Size(106, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Seleccione al Cliente";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(292, 36);
+            this.label2.Location = new System.Drawing.Point(219, 29);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(144, 16);
+            this.label2.Size = new System.Drawing.Size(115, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Seleccione el Vehiculo";
             // 
             // cbxcliente
             // 
             this.cbxcliente.FormattingEnabled = true;
-            this.cbxcliente.Location = new System.Drawing.Point(38, 65);
+            this.cbxcliente.Location = new System.Drawing.Point(28, 53);
+            this.cbxcliente.Margin = new System.Windows.Forms.Padding(2);
             this.cbxcliente.Name = "cbxcliente";
-            this.cbxcliente.Size = new System.Drawing.Size(160, 24);
+            this.cbxcliente.Size = new System.Drawing.Size(121, 21);
             this.cbxcliente.TabIndex = 3;
             // 
             // cbxvehiculo
             // 
             this.cbxvehiculo.FormattingEnabled = true;
-            this.cbxvehiculo.Location = new System.Drawing.Point(279, 65);
+            this.cbxvehiculo.Location = new System.Drawing.Point(209, 53);
+            this.cbxvehiculo.Margin = new System.Windows.Forms.Padding(2);
             this.cbxvehiculo.Name = "cbxvehiculo";
-            this.cbxvehiculo.Size = new System.Drawing.Size(166, 24);
+            this.cbxvehiculo.Size = new System.Drawing.Size(126, 21);
             this.cbxvehiculo.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(63, 169);
+            this.label3.Location = new System.Drawing.Point(56, 104);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.Size = new System.Drawing.Size(75, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Ingrese Precio";
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(50, 208);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(312, 134);
+            this.listBox1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 7;
+            // 
             // Venta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 450);
+            this.ClientSize = new System.Drawing.Size(423, 366);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cbxvehiculo);
             this.Controls.Add(this.cbxcliente);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Venta";
             this.Text = "Venta";
             this.ResumeLayout(false);
@@ -114,5 +142,7 @@
         private System.Windows.Forms.ComboBox cbxcliente;
         private System.Windows.Forms.ComboBox cbxvehiculo;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Trabajo_CS/Venta.cs
+++ Trabajo_CS/Venta.cs
@@ -16,5 +16,11 @@ namespace Trabajo_CS
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
