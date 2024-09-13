3c221970b5d2aec60d28b1e7859f2e978d8e9bbc
[Arthur] <115912843+Artthur0@users.noreply.github.com>
2024-09-12 23:11:27 -0300

    algo

--- Trabajo_CS/Venta.Designer.cs
+++ Trabajo_CS/Venta.Designer.cs
@@ -41,10 +41,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(232, 121);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(309, 149);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(67, 29);
+            this.button1.Size = new System.Drawing.Size(89, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "Guardar";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,72 +53,71 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 29);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(51, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.Size = new System.Drawing.Size(133, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Seleccione al Cliente";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(219, 29);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(292, 36);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.Size = new System.Drawing.Size(144, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Seleccione el Vehiculo";
             // 
             // cbxcliente
             // 
             this.cbxcliente.FormattingEnabled = true;
-            this.cbxcliente.Location = new System.Drawing.Point(28, 53);
-            this.cbxcliente.Margin = new System.Windows.Forms.Padding(2);
+            this.cbxcliente.Location = new System.Drawing.Point(37, 65);
+            this.cbxcliente.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbxcliente.Name = "cbxcliente";
-            this.cbxcliente.Size = new System.Drawing.Size(121, 21);
+            this.cbxcliente.Size = new System.Drawing.Size(160, 24);
             this.cbxcliente.TabIndex = 3;
             // 
             // cbxvehiculo
             // 
             this.cbxvehiculo.FormattingEnabled = true;
-            this.cbxvehiculo.Location = new System.Drawing.Point(209, 53);
-            this.cbxvehiculo.Margin = new System.Windows.Forms.Padding(2);
+            this.cbxvehiculo.Location = new System.Drawing.Point(279, 65);
+            this.cbxvehiculo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbxvehiculo.Name = "cbxvehiculo";
-            this.cbxvehiculo.Size = new System.Drawing.Size(126, 21);
+            this.cbxvehiculo.Size = new System.Drawing.Size(167, 24);
             this.cbxvehiculo.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(56, 104);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(75, 128);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.Size = new System.Drawing.Size(94, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Ingrese Precio";
             // 
             // tb_dinero
             // 
-            this.tb_dinero.Location = new System.Drawing.Point(28, 130);
+            this.tb_dinero.Location = new System.Drawing.Point(37, 160);
+            this.tb_dinero.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tb_dinero.Name = "tb_dinero";
-            this.tb_dinero.Size = new System.Drawing.Size(128, 20);
+            this.tb_dinero.Size = new System.Drawing.Size(169, 22);
             this.tb_dinero.TabIndex = 7;
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(28, 195);
+            this.listView1.Location = new System.Drawing.Point(37, 240);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(312, 134);
+            this.listView1.Size = new System.Drawing.Size(415, 164);
             this.listView1.TabIndex = 8;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Venta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 366);
+            this.ClientSize = new System.Drawing.Size(505, 450);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.tb_dinero);
             this.Controls.Add(this.label3);
@@ -127,7 +126,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Venta";
             this.Text = "Venta";
             this.ResumeLayout(false);
